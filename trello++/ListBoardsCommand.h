ceb2c75fd6d22a9e9d5bfda826fbf05fffd37111
kadir malak <kadirmalak@gmail.com>
2017-10-27 20:58:49 +0300

    trello api class init

--- trello++/ListBoardsCommand.h
+++ trello++/ListBoardsCommand.h
@@ -1,72 +1,21 @@
 #pragma once
 
 #include <iostream>
-#include <curl/curl.h>
-
-#include "rapidjson/document.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/stringbuffer.h"
 
 #include "AbstractCommand.h"
 #include "Config.h"
-
-using namespace rapidjson;
+#include "TrelloApi.h"
 
 using namespace std;
-
-string response;
-
-size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
-{
-	size_t len = size * nmemb;
-	response.append(ptr, len);
-	return len;
-}
 
 class ListBoardsCommand : public AbstractCommand {
 public:
 	ListBoardsCommand() : AbstractCommand("list boards") {}
 
-	// TODO: implement
+	// TODO: implement, refactor
 	virtual void operator()() {
 		cout << "listing boards..." << endl;
-
-		CURL *curl;
-		CURLcode res;
-
-		curl_global_init(CURL_GLOBAL_DEFAULT);
-
-		string URL = string{ TRELLO_API_BASE "/members/me/boards?key=" TRELLO_KEY "&token=" TRELLO_TOKEN };
-
-		curl = curl_easy_init();
-		if (curl) {
-			curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-
-			/* Perform the request, res will get the return code */
-			res = curl_easy_perform(curl);
-			/* Check for errors */
-			if (res != CURLE_OK)
-			{
-				fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-			}
-			else
-			{
-				Document d;
-				d.Parse(response.c_str());
-
-				if (!d.HasParseError()) {
-					for (auto & listVal : d.GetArray())
-					{
-						fprintf(stdout, " - %s\n", listVal.GetObjectW()["name"].GetString());
-					}
-				}
-			}
-
-			/* always cleanup */
-			curl_easy_cleanup(curl);
-		}
-
-		curl_global_cleanup();
+		auto api = TrelloApi(TRELLO_API_BASE, TRELLO_KEY, TRELLO_TOKEN);
+		api.ListBoards();
 	}
 };
--- trello++/TrelloApi.h
+++ trello++/TrelloApi.h
@@ -1,18 +1,78 @@
 #pragma once
 
 #include <string>
+#include <curl/curl.h>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
+using namespace rapidjson;
 using namespace std;
+
+// TODO: don't use global variable here
+string response;
+
+// TODO: don't use global function here
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
+{
+	size_t len = size * nmemb;
+	response.append(ptr, len);
+	return len;
+}
 
 class TrelloApi {
 public:
-	TrelloApi(const string & apiBase) : _apiBase{ apiBase } {
-
+	TrelloApi(const string & apiBase, const string & key, const string & token) 
+		: _apiBase{ apiBase }, _key{ key }, _token{ token } {
+		curl_global_init(CURL_GLOBAL_DEFAULT);
+		curl = curl_easy_init();
+		if (curl) {
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+		}
 	}
 
-	void ListBoards(const string & username) {
-		string url = _apiBase + "/members/" + username + "/boards";
+	~TrelloApi() {
+		if (curl) {
+			curl_easy_cleanup(curl);
+		}
+		curl_global_cleanup();
+	}
 
+	void ListBoards() {
+		if (!curl) {
+			fprintf(stderr, "curl is not ready\n");
+			return;
+		}
+
+		//response.clear();
+		// TODO: clear response buffer here
+		string url = _apiBase + "/members/me/boards?key=" + _key + "&token=" + _token;
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		res = curl_easy_perform(curl);
+		/* Check for errors */
+		if (res != CURLE_OK)
+		{
+			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+			return;
+		}
+		
+		Document d;
+		d.Parse(response.c_str());
+
+		if (d.HasParseError()) {
+			fprintf(stderr, "could not parse response\n");
+			return;
+		}
+
+		for (auto & listVal : d.GetArray())
+		{
+			fprintf(stdout, " - %s\n", listVal.GetObjectW()["name"].GetString());
+		}
 	}
 private:
 	string _apiBase;
+	string _key;
+	string _token;
+	CURL *curl;
+	CURLcode res;
 };
